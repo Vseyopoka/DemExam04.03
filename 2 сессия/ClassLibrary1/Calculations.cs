d0224f769025d54b5357d1701ac7372537e25b9a
Vseyopoka <88979663+Vseyopoka@users.noreply.github.com>
2023-03-04 14:13:48 +0500

    Add files via upload

---
+++ 2 сессия/ClassLibrary1/Calculations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Calculations
+    {
+        public TimeSpan[] AvailablePeriods(TimeSpan[] startTimes,int[] durations, TimeSpan beginWorkingTime, TimeSpan endWorkingTime, int consultationTime)
+        {
+          return startTimes;
+
+        }
+
+    }
+}
